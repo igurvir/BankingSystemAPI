dcbe7ea960d27216b84a8a4d512d90b0b4804a78
igurvir <gurvirsingh0504@gmail.com>
2024-09-12 01:59:52 -0400

    Initial commit: Project setup with Spring Boot, JPA, PostgreSQL

---
+++ src/main/java/com/bankingsystem/bankingsystem/BankingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bankingsystem.bankingsystem;
+
+/**
+ *
+ * @author gurvir
+ */
+public class BankingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
